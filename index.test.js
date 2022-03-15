4b4542d61da0ee515e2e641b87bc03594a65e813
Futahei <futahei1108@icloud.com>
2022-03-15 02:59:45 +0000

    fix test

--- index.test.js
+++ index.test.js
@@ -17,7 +17,7 @@ test('wait 500 ms', async () => {
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
+  process.env['who-to-greet'] = "test";
   const ip = path.join(__dirname, 'index.js');
   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
   console.log(result);
