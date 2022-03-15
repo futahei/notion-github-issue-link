307cb4a8dbd31d9be2cefc37a245e225c229c1c6
Futahei <futahei1108@icloud.com>
2022-03-15 02:28:46 +0000

    add test actions

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function run() {
+  try {
+    const name = core.getInput('who-to-greet')
+    console.log(`Hello ${name}!`)
+    const time = new Date().toTimeString()
+    core.setOutput('time', time)
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
+
+/*
 const core = require('@actions/core');
 const wait = require('./wait');
 
@@ -19,3 +39,4 @@ async function run() {
 }
 
 run();
+*/
